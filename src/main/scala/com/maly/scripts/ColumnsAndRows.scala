1f0149184933bf91e22022875285fa8fd2999103
jmarusak <jmarusak@users.noreply.github.com>
2024-04-03 15:37:02 -0400

    to work with vscode

---
+++ src/main/scala/com/maly/scripts/ColumnsAndRows.scala
@@ -0,0 +1,13 @@
+import org.apache.spark.sql.{SparkSession, Row}
+import org.apache.spark.sql.functions.{col, expr}
+import org.apache.spark.sql.types.{StructType, StructField, StringType, IntegerType}
+
+object Script {
+  val spark = SparkSession.getActiveSession match {
+    case Some(session) => session
+    case None => SparkSession.builder().master("local[1]").getOrCreate()
+  }
+
+  val df = spark.read.format("json").load("data/flight-data/json/2015-summary.json")
+  df.show(5)
+}
