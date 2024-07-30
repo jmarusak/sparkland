54b5ee756364d4ba5b00808a85c92acee77658e6
jmarusak <jmarusak@users.noreply.github.com>
2024-07-30 10:54:25 -0400

    spark columns

--- src/main/scala/sparkland/MainApp.scala
+++ src/main/scala/sparkland/MainApp.scala
@@ -1,14 +1,19 @@
 import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.Row 
+import org.apache.spark.sql.functions.{col, expr}
 
 object MainApp {
   def main(args: Array[String]): Unit = {
-    val spark = SparkSession.getActiveSession match {
+    val session = SparkSession.getActiveSession match {
       case Some(session) => session
       case None => SparkSession.builder().master("local[1]").getOrCreate()
     }
-    spark.conf.set("spark.sql.shuffle.partitions", 3)
+    session.conf.set("spark.sql.shuffle.partitions", 3)
 
-    val df = spark.read.format("json").load("data/flight-data/json/2015-summary.json")
-    df.printSchema()
+    val df = session.read.format("json").load("data/flight-data/json/2015-summary.json")
+    val row = df.first()
+    
+    val aiport = row(0)
+    println(aiport.getClass)
   }
 }
